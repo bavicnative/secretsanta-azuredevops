79b6f948a0a4157f089b15178cf98135cc5c6271
Bavithran M <79276699+bavicnative@users.noreply.github.com>
2025-03-21 22:21:52 +0530

    devops pipeline with vars

--- AzureDevOps/Infrastructure/azure-vm/variables.tf
+++ AzureDevOps/Infrastructure/azure-vm/variables.tf
@@ -1,3 +1,24 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Service Principal Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Service Principal Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
 variable "resource_group_name" {
   description = "Resource group name"
   default     = "careerbytecode"
